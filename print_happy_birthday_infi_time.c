dcd5b8278e62769a05aba9cd51880915f296ca79
Hritik <ritikmily143@gmail.com>
2022-04-11 00:38:31 +0530

    print happy birthday non stop
    print happy birthday with under line using for loop non stop

---
+++ print_happy_birthday_infi_time.c
@@ -0,0 +1,70 @@
+/*-------------------------Docuentation Section ----------------------------*/
+/*      for print pattern using for loop
+         -------------------------------------------------
+                       Happy Birth Day
+         -------------------------------------------------    */
+/*------------------------Preprocessor Directives --------------------------*/
+
+#include <stdio.h> //cotains the pintf(),scanf(),getchar() build in function
+#include <conio.h> //contains the getch(),getche() build in function
+#include <stdlib.h>
+
+/*---------------- Prototype Function declaration------------------*/
+void hold_it(void);
+void college(void);
+void loop(void);
+void repet(void);
+void warning(void);
+
+/*------------------------------------------------------*/
+int main()
+{
+    system("cls");
+    warning();
+    repet();
+    hold_it();
+    return 0;
+}
+/*------------------------------------------------------*/
+void college()
+{
+    printf("\n");
+    printf("\t\a\t   Happy Birth Day ");
+    printf("\n\n");
+}
+/*------------------------------------------------------*/
+void loop()
+{
+    printf("\t");
+    for (int i = 0; i < 45; i++)
+    {
+        printf("-");
+    }
+    printf("\n");
+    return;
+}
+void repet(void)
+{
+    for (;;)
+    {
+        loop();
+        college();
+        loop();
+    }
+    return;
+}
+void warning(void)
+{
+    printf("\n\tWARNING !!!!!!!\n\t:This loop will run until you terminate it\n\t:for terminate loop ,Press Ctrl+C");
+    printf("\n\tPress Enter To Continue\n");
+    getchar();
+    return;
+}
+/*------------------------------------------------------*/
+void hold_it(void)
+{
+    printf("\n\n\t");
+    system("pause");
+    return;
+}
+/*------------------------------------------------------*/
