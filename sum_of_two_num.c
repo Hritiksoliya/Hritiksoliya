6a1a5ea80a95941b4331c510ec570f587213d2c0
Hritik <ritikmily143@gmail.com>
2022-04-11 00:43:09 +0530

    sum of two number in c
    sum of two number in c languge

---
+++ sum_of_two_num.c
@@ -0,0 +1,22 @@
+#include <stdio.h>// contains the printf(),scanf(),getch(),putch()built in a function
+#include <conio.h>// contain the getch(),getche(),built in function.
+#include <stdlib.h>
+/*---------------------------------------------------------------*/
+int main()
+{
+    int value1, value2, sum;
+    value1 = 20;
+    value2 = 25;
+    sum = value1 + value2;
+    system("cls");
+    // polytechnic collage
+    printf("\n\n hello worid");
+    printf("\n\n---------------------------------------------------------------");
+    printf("\n\n\awelcom to the c language programing");
+
+    printf("\nthe sum of %d and %d is %d\n", value1, value2, sum);
+    printf("\n---------------------------------------------------------------");
+    getch();
+    return 0;
+
+}
