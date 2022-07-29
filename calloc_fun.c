9c88aa2bd86b16aba89feed930ae5992964baae0
Hritik <ritikmily143@gmail.com>
2022-07-29 21:29:18 +0530

    Create calloc_fun.c
    how does work calloc function

---
+++ calloc_fun.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+    int i,n,*ptr;
+ 
+    printf("Enter Number of Elements : ");
+    scanf("%d",&n);
+ 
+    ptr=(int *)calloc(n,sizeof(int));
+ 
+    if(ptr==NULL)
+    {
+        printf("Memory Not Allocated");
+    }
+    else
+    {
+        for(i=0;i<n;i++)
+        {
+            printf("Enter Element : ");
+            scanf("%d",ptr+i);
+        }
+ 
+        printf("All Elements : ");
+        for(i=0;i<n;i++)
+        {
+            printf("%d ",*(ptr+i));
+        }
+ 
+        free(ptr);
+    }
+ 
+    return 0;
+}
